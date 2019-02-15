572cfca76bc5891add0534285baa7c8b10d165ca
usuario <usuario@PC-112>
2019-02-15 09:51:01 +0100

    hola

--- src/practicagit/PracticaGit.java
+++ src/practicagit/PracticaGit.java
@@ -16,6 +16,7 @@ public class PracticaGit {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        //VIVA ESPAÑA CARAJO!!!!!!!!!
     }
     
 }
